547d0047663fe9f4d5fd04bd55594e19c914cace
rahafdm <rahaf.nasser.abt.js@tuwaiq.edu.sa>
2021-12-04 14:36:16 +0300

    books react

---
+++ src/componets/BookItem.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { Col,  Card, Button} from "react-bootstrap"
+import BooksContext from "../Utils/BooksContext";
+
+function BookItem(props) {
+    const { book,  inProfile} = props
+    const {editBook, deleteBook,} = useContext(BooksContext)
+    return ( 
+
+        <Col>
+      <Card>
+        <Card.Img variant="top" src={book.image} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text> {book.description}</Card.Text>
+          <Card.Text>{book.auther}  </Card.Text>
+
+          {inProfile === true ? (
+            <>
+              <Button variant="info" id={book._id} onClick={editBook}>
+                Edit
+              </Button>
+              <Button className="ms-2" variant="danger" id={book._id} onClick={deleteBook}>
+                Delete
+              </Button>
+            </>
+          ) : null}
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
+
+
+export default BookItem; 
---
+++ src/pages/AddBook.js
@@ -0,0 +1,59 @@
+import { Form, Col, Row, Button } from "react-bootstrap"
+import {useContext } from "react";
+import BooksContext from "../Utils/BooksContext"
+function AddBook() {
+  const { addProduct, setErrorSignUp } = useContext(BooksContext)
+  setErrorSignUp(null)
+  return (
+    <div className="ms-4 mt-4">
+      <h1>Add Books</h1>
+      <Form className="mt-5" onSubmit={addProduct}>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column md="2">
+            Title
+          </Form.Label>
+          <Col sm="6">
+            <Form.Control type="text" name="title" required />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column md="2">
+            Description
+          </Form.Label>
+          <Col sm="6">
+            <Form.Control type="text" name="description" required />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column md="2">
+            auther
+          </Form.Label>
+          <Col sm="6">
+            {/* <Form.Control type="number" name="price" required /> */}
+            <Form.Control type="text" name="auther" required />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label column md="2">
+            Image
+          </Form.Label>
+          <Col sm="6">
+            <Form.Control type="url" name="image" required />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="my-5">
+          <Col md={{ span: 10, offset: 2 }}>
+            <Button type="submit" variant="primary">
+              Add Books
+            </Button>
+          </Col>
+        </Form.Group>
+      </Form>
+    </div>
+  )
+}
+
+export default AddBook
