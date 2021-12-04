94fc71cd6e74595d2a481a1b7a6e2298c62e2da9
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-12-04 20:23:19 +0300

    done HW

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import BookGroup from "../components/BookGroup"
+
+function Home(props) {
+  const { books, editBook, editId, confirmEdit, deleteBook, setErrorSignup } = props
+  setErrorSignup(null)
+  return (
+    <>
+      <h1>Books</h1>
+      <BookGroup books={books} editBook={editBook} editId={editId} confirmEdit={confirmEdit} deleteBook={deleteBook} />
+    </>
+  )
+}
+
+export default Home
